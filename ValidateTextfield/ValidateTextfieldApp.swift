86215e9e829f302a7609a27b04b4b2772402b6cc
Tan Tran <tantan992@gmail.com>
2023-02-20 11:20:30 -0500

    Initial Commit

---
+++ ValidateTextfield/ValidateTextfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidateTextfieldApp.swift
+//  ValidateTextfield
+//
+//  Created by Tan Tan on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ValidateTextfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
